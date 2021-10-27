6655af964f0b264bc46cd64d79f54c7f87af9352
birool <33376876+birool@users.noreply.github.com>
2021-10-27 23:05:55 +0300

    Initial Author Routes

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ if(process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -13,6 +15,8 @@ app.set('layout', 'layouts/layout')
 
 app.use(expressLayouts)
 app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL,  {
@@ -25,4 +29,6 @@ db.once('open', () => console.log('connected to MongoDB'))
 
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+
 app.listen(process.env.PORT || 3000)
